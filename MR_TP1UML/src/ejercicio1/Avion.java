01cabd60b6ba500c1378054b1c93ecd4797ab5f4
Mili Ranaldi <mranaldi@gmail.com>
2024-03-21 15:20:22 -0300

    --

--- MR_TP1UML/src/ejercicio1/Avion.java
+++ MR_TP1UML/src/ejercicio1/Avion.java
@@ -1,2 +1,49 @@
-package ejercicio1;public class Avion {
+package ejercicio1;
+
+public class Avion {
+    private String nombreA;
+    private int cantPasajeros;
+    private int capMaletas;
+    private Vuelo vuelo;
+    public Avion() {
+    }
+
+    public Avion(String nombreA,int cantPasajeros, int capMaletas, Vuelo v1) {
+        this.nombreA=nombreA;
+        this.cantPasajeros=cantPasajeros;
+        this.capMaletas = capMaletas;
+        this.vuelo= v1;
+    }
+
+    public String getNombreA() {
+        return nombreA;
+    }
+
+    public void setNombreA(String nombreA) {
+        this.nombreA = nombreA;
+    }
+
+    public int getCantPasajeros() {
+        return cantPasajeros;
+    }
+
+    public void setCantPasajeros(int cantPasajeros) {
+        this.cantPasajeros = cantPasajeros;
+    }
+
+    public int getCapMaletas() {
+        return capMaletas;
+    }
+
+    public void setCapMaletas(int capMaletas) {
+        this.capMaletas = capMaletas;
+    }
+
+    public Vuelo getVuelo() {
+        return vuelo;
+    }
+
+    public void setVuelo(Vuelo vuelo) {
+        this.vuelo = vuelo;
+    }
 }
