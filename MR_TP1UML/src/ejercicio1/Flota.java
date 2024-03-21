b12852502dd13896f51e8efc9fb1242dc36d8cb1
Mili Ranaldi <mranaldi@gmail.com>
2024-03-21 15:22:23 -0300

    --

--- MR_TP1UML/src/ejercicio1/Flota.java
+++ MR_TP1UML/src/ejercicio1/Flota.java
@@ -1,2 +1,31 @@
-package ejercicio1;public class Flota {
-}
+package ejercicio1;
+
+
+
+
+    public class Flota {
+
+        private Avion avion ;/*agregacion por composicion */
+
+        public Flota(Avion avion) {
+
+            this.avion= avion;
+        }
+
+        public Avion getAvion() {
+            return avion;
+        }
+
+        public void setAvion(Avion avion) {
+            this.avion = avion;
+        }
+        public void datosAvion(){
+            System.out.println("Datos del avion: ");
+            System.out.println("Nombre del avion: "+avion.getNombreA());
+            System.out.println("Cantidad max pasajeros: "+avion.getCantPasajeros());
+            System.out.println("Cantidad max de maletas: "+avion.getCapMaletas());
+
+        }
+    }
+
+
