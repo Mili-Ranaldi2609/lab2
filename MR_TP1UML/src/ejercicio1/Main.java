e5667d11440f530f04789a9b23b0205e25091c2b
Mili Ranaldi <mranaldi@gmail.com>
2024-03-21 15:32:35 -0300

    --

--- MR_TP1UML/src/ejercicio1/Main.java
+++ MR_TP1UML/src/ejercicio1/Main.java
@@ -1,2 +1,21 @@
-package ejercicio1;public class Main {
+package ejercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Vuelo v1= new Vuelo("Andesmar","Mendoza","Buenos Aires");
+        Avion a1= new Avion("ee123uu",45,100,v1);
+        Flota flota = new Flota(a1);
+        Reserva reserva = new Reserva("WAfy123", 0001);
+        reserva.agregarVuelo(v1);
+        reserva.mostrarDatos();
+        Piloto pilot1= new Piloto(124,"Daniel Medina",44437603,31);
+        pilot1.datosPiloto();
+        flota.datosAvion();
+        System.out.println("-------------------------------------------");
+        pilot1.volar();
+        pilot1.caminar();
+
+
+    }
 }
