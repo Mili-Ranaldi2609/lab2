d8892bc3c1f051c9052774df7b96a1619834d77c
Mili Ranaldi <mranaldi@gmail.com>
2024-03-21 15:37:40 -0300

    --

--- MR_TP1UML/src/ejercicio2/Pasajero.java
+++ MR_TP1UML/src/ejercicio2/Pasajero.java
@@ -1,2 +1,29 @@
-package ejercicio2;public class Pasajero {
+package ejercicio2;
+
+public class Pasajero extends Persona{
+    private int nroPasaje;
+    private String asiento;
+
+    public Pasajero(String nombre, int dni, int edad,int nroPasaje, String asiento) {
+        super(nombre, dni, edad);
+        this.nroPasaje=nroPasaje;
+        this.asiento = asiento;
+    }
+
+    public String getAsiento() {
+        return asiento;
+    }
+
+    public void setAsiento(String asiento) {
+        this.asiento = asiento;
+    }
+
+    public int getNroPasaje() {
+        return nroPasaje;
+    }
+
+    public void setNroPasaje(int nroPasaje) {
+        this.nroPasaje = nroPasaje;
+    }
+
 }
