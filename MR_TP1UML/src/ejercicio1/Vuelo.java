0ed883274bb68b800880d9fe543331e61cefd368
Mili Ranaldi <mranaldi@gmail.com>
2024-03-21 15:23:22 -0300

    --

--- MR_TP1UML/src/ejercicio1/Vuelo.java
+++ MR_TP1UML/src/ejercicio1/Vuelo.java
@@ -1,2 +1,43 @@
-package ejercicio1;public class Vuelo {
+package ejercicio1;
+
+public class Vuelo {
+    private String compañia;
+    private String origen;
+    private String destino;
+
+
+
+    public Vuelo(String compañia, String origen, String destino) {
+        this.compañia = compañia;
+        this.origen=origen;
+        this.destino=destino;
+
+
+    }
+
+    public String getCompañia() {
+        return compañia;
+    }
+
+    public void setCompañia(String compañia) {
+        this.compañia = compañia;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+
 }
