b8409db0c72a47b624869061b1fb44a0a3bfc87c
Mili Ranaldi <mranaldi@gmail.com>
2024-03-21 15:34:59 -0300

    --

--- MR_TP1UML/src/ejercicio1/Reserva.java
+++ MR_TP1UML/src/ejercicio1/Reserva.java
@@ -1,2 +1,50 @@
-package ejercicio1;public class Reserva {
+package ejercicio1;
+
+public class Reserva {
+    private String codR;
+    private int nroR;
+    private Vuelo vuelo;/*asociacion por agregacion*/
+
+    public Reserva(String codR, int nroR) {
+        this.codR= codR;
+        this.nroR= nroR;
+
+    }
+
+    public String getCodR() {
+        return codR;
+    }
+
+    public void setCodR(String codR) {
+        this.codR = codR;
+    }
+
+    public void setNroR(int nroR) {
+        this.nroR = nroR;
+    }
+
+    public int getNroR() {
+        return nroR;
+    }
+
+    public Vuelo getVuelo() {
+        return vuelo;
+    }
+
+    public void agregarVuelo(Vuelo vuelo){
+        this.vuelo=vuelo;
+
+    };/*asociacion por agregacion*/
+    public void mostrarDatos(){
+        System.out.println("Datos de reserva de vuelo: ");
+        System.out.println("Cod Reserva: "+codR);
+        System.out.println("Nro Reserva: "+nroR);
+        System.out.println("Compañia: "+vuelo.getCompañia());
+        System.out.println("Origen: "+vuelo.getOrigen());
+        System.out.println("Destino: "+vuelo.getDestino());
+
+
+
+    }
+
 }
