9f8a2340a75cb65f7d7a4560ba96aa94835fbda7
Mili Ranaldi <mranaldi@gmail.com>
2024-03-21 15:33:36 -0300

    --

--- MR_TP1UML/src/ejercicio1/Persona.java
+++ MR_TP1UML/src/ejercicio1/Persona.java
@@ -1,2 +1,40 @@
-package ejercicio1;public class Persona {
+package ejercicio1;
+
+public class Persona {
+    private String nombre;
+    private int edad;
+    private int dni;
+
+    public Persona(String nombre, int edad, int dni) {
+        this.nombre=nombre;
+        this.edad=edad;
+        this.dni = dni;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void caminar(){
+        System.out.println("Yo camino");
+    };
 }
