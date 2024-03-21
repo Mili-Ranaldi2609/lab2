0afe96a6431fc9f0fb6984d87300b6c0f1f0a692
Mili Ranaldi <mranaldi@gmail.com>
2024-03-21 15:39:24 -0300

    --

--- MR_TP1UML/src/ejercicio2/Piloto.java
+++ MR_TP1UML/src/ejercicio2/Piloto.java
@@ -1,2 +1,30 @@
-package ejercicio2;public class Piloto {
+package ejercicio2;
+
+public class Piloto extends Persona{
+    private int nroLegajo;
+    private int licencia;
+
+    public Piloto(String nombre, int dni, int edad,int nroLegajo, int licencia) {
+        super(nombre, dni, edad);
+        this.nroLegajo=nroLegajo;
+        this.licencia = licencia;
+    }
+
+    public int getLicencia() {
+        return licencia;
+    }
+
+    public void setLicencia(int licencia) {
+        this.licencia = licencia;
+    }
+
+    public int getNroLegajo() {
+        return nroLegajo;
+    }
+
+    public void setNroLegajo(int nroLegajo) {
+        this.nroLegajo = nroLegajo;
+    }
+
+
 }
