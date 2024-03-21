81955503603fcfeb8e58577f1a2a13093d40864e
Mili Ranaldi <mranaldi@gmail.com>
2024-03-21 15:34:12 -0300

    --

--- MR_TP1UML/src/ejercicio1/Piloto.java
+++ MR_TP1UML/src/ejercicio1/Piloto.java
@@ -1,2 +1,30 @@
-package ejercicio1;public class Piloto {
+package ejercicio1;
+
+public class Piloto extends Persona implements Volador {
+    private int nroLegajo;
+
+    public Piloto(int nroLegajo,String nombre,int dni,int edad) {
+        super(nombre,dni,edad);
+        this.nroLegajo = nroLegajo;
+    }
+
+    public int getNroLegajo() {
+        return nroLegajo;
+    }
+
+    public void setNroLegajo(int nroLegajo) {
+        this.nroLegajo = nroLegajo;
+    }
+    public void datosPiloto(){
+        System.out.println("Los datos del piloto son: ");
+        System.out.println("Nro de Legajo: "+nroLegajo);
+        System.out.println("Dni: "+getDni());
+        System.out.println("Nombre: "+getNombre());
+        System.out.println("Edad: "+getEdad());
+    }
+    @Override
+    public void volar(){
+        System.out.println("Yo vuelo piloteando un avion");
+    }
+
 }
