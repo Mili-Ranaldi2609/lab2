865b053a6456759dbbba0bbd1b874ffd9cd351d0
Mili Ranaldi <mranaldi@gmail.com>
2024-04-16 16:55:21 -0300

    ----

--- tpExcepciones/src/Ejer10/miException.java
+++ tpExcepciones/src/Ejer10/miException.java
@@ -1,2 +1,15 @@
-package Ejer10;public class miException {
+package Ejer10;
+
+public class miException extends Exception{
+    private String objetoQueLaProduce;
+
+    public miException(String mensaje, String objetoQueLaProduce) {
+        super(mensaje);
+        this.objetoQueLaProduce = objetoQueLaProduce;
+    }
+
+    public String getObjetoQueLaProduce() {
+        return objetoQueLaProduce;
+    }
+
 }
--- tpExcepciones/src/ejer12/Main.java
+++ tpExcepciones/src/ejer12/Main.java
@@ -1,2 +1,36 @@
-package ejer12;public class Main {
+package ejer12;
+
+public class Main extends Exception{
+    public static void main(String[] args) {
+        try {
+            metodoUno();
+        } catch (Exception e) {
+            System.out.println("Excepción capturada en el método main: " + e.getMessage());
+        }
+    }
+
+    public static void metodoUno() {
+        System.out.println("Inicio del método uno");
+        metodoDos();
+        System.out.println("Fin del método uno");
+    }
+
+    public static void metodoDos() {
+        System.out.println("Inicio del método dos");
+        metodoTres();
+        System.out.println("Fin del método dos");
+    }
+
+    public static void metodoTres() {
+        System.out.println("Inicio del método tres");
+        metodoCuatro();
+        System.out.println("Fin del método tres");
+    }
+
+    public static void metodoCuatro() {
+        System.out.println("Inicio del método cuatro - lanzando excepción");
+        throw new RuntimeException("Excepción lanzada desde el método cuatro");
+    }//el método metodoCuatro lanza una excepción RuntimeException. Esta excepción
+    // se propaga hacia arriba a través de la pila de llamadas
+    // de métodos metodoTres, metodoDos y metodoUno
 }
